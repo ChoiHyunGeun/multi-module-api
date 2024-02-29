be50a75881110458dd61ccba25fce0cd47a5ecb3
hgChoi <chlgusrms96@naver.com>
2024-02-29 17:04:42 +0900

    DB연동, 저장, 조회 로직 구현

--- src/main/java/dev/be/moduleapi/service/DemoService.java
+++ src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,7 +1,9 @@
 package dev.be.moduleapi.service;
 
 import dev.be.moduleapi.exception.CustomException;
+import dev.be.modulecommon.domain.Member;
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.repositories.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class DemoService {
     private final CommonDemoService commonDemoService;
+
+    private final MemberRepository memberRepository;
     public String save() {
+        memberRepository.save(Member.builder()
+                .name("나다")
+                .build());
         System.out.println(CodeEnum.SUCCESS.getCode());
         System.out.println(commonDemoService.commonService());
         return "save";
     }
 
     public String find() {
+        System.out.println(memberRepository.findAll().size());
         return "find";
     }
 
